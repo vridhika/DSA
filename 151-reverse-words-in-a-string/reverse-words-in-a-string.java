9ef98c8b61d3b0c040d9c996325e148eedcfdfba
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2025-06-30 08:34:00 +0530

    Update reverse-words-in-a-string.java

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String reverseWords(String s) {
         String[] words = s.split(" +");
         StringBuilder result = new  StringBuilder();
-        for(int i = words.length - 1; i >= 0; i--) {
+        for(int i = words.length - 1; i >= 0; i--) {  //loop that goes from last word to first
             result.append(words[i]);
             if(i != 0)
                 result.append(" ");
