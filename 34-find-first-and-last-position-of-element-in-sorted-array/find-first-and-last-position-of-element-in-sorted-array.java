7efa70f44fd1baa64756845eeb0f65a0dc6acfcd
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2025-08-12 11:57:06 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (8.79%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,45 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int left = findLeftBound(nums, target);
-        int right = findRightBound(nums, target);
-        return new int[]{left, right};
+        int left=findLeftBound(nums,target);
+        int right=findRightBound(nums,target);
+        return new int[]{left,right};
     }
-
-    private int findLeftBound(int[] nums, int target) {
-        int index = -1, low = 0, high = nums.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                index = mid;       // Found target, but keep going left
-                high = mid - 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+    private int findLeftBound(int[]nums,int target){
+        int index=-1;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                index=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target){
+               low=mid+1;
+            }
+            else{
+                high=mid-1;
             }
         }
         return index;
     }
-
-    private int findRightBound(int[] nums, int target) {
-        int index = -1, low = 0, high = nums.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                index = mid;       // Found target, but keep going right
-                low = mid + 1;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+    private int findRightBound(int[]nums,int target){
+        int index=-1;
+        int low=0;
+        int high=nums.length-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                index=mid;
+                low=mid+1;
             }
+            else if(nums[mid]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+            
         }
         return index;
     }
