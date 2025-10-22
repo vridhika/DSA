489882ff79956bf2ee45d5a4a63e42ccdddefc63
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2025-10-22 21:50:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (62.70%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -5,14 +5,20 @@ class Solution {
         int j=n-1;
         while(i>=0&&j>=0){
             if(A[i]>=B[j]){
-             A[idx--]=A[i--];
+                A[idx]=A[i];
+                idx--;
+                i--;
             }
             else{
-                A[idx--] = B[j--];
+                A[idx]=B[j];
+                idx--;
+                j--;
             }
         }
         while(j>=0){
-            A[idx--]=B[j--];
+            A[idx]=B[j];
+            idx--;
+            j--;
         }
     }
-};
+}
