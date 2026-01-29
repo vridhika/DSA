5776999e28e2c1286205f2161915114577e1856f
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2026-01-29 13:46:10 +0530

    Time: 14 ms (32.66%) | Memory: 46.4 MB (31.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,28 +1,12 @@
 class Solution {
-    boolean isAlphaNumeric(char ch){
-        if((ch>='0' && ch<='9') || (ch>='a'&& ch<='z') || (ch>='A' && ch<='Z')){
-            return true;
-        }
-        return false;
-    }
     public boolean isPalindrome(String s) {
-        int start=0;
-        int end=s.length()-1;
-        while(start<end){
-            if(!isAlphaNumeric(s.charAt(start))){
-                start++;
-                continue;
-            }
-            if(!isAlphaNumeric(s.charAt(end))){
-                end--;
-                continue;
-            }
-            if(Character.toLowerCase(s.charAt(start))!=Character.toLowerCase(s.charAt(end))){
+        s=s.toLowerCase();
+        s=s.replaceAll("[^a-z0-9]","");
+        int n=s.length();
+        for(int i=0;i<n/2;i++){
+            if(s.charAt(i)!=s.charAt(n-i-1)){
                 return false;
             }
-                start++;
-                end--;
-            
         }
         return true;
     }
