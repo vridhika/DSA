672d46e65b008ae8263f27791bc451f0ded291ab
Vridhika Nag <147415302+vridhika@users.noreply.github.com>
2026-02-22 17:40:14 +0530

    Time: 5 ms (20.87%) | Memory: 47.6 MB (18.62%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        for(int i=0;i<=nums.length;i++){
+          if(!set.contains(i)){
+            return i;
+          }
+        }
+        return -1;
+    }
+}
